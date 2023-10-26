1488214196f4232394be622d897ae98a937fcae7
Pedro_Yago_Iraiore_Carvalho <yagoiraiore@gmail.com>
2023-10-26 01:55:31 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'api_service.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
+  final ApiService apiService = ApiService();
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Posts')),
+            body: FutureBuilder<List<dynamic>>(
+                future: apiService.Posts(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        final post = snapshot.data![index];
+                        return ListTile(
+                          title: Text(post['title']),
+                          subtitle: Text(post['body']),
+                        );
+                      },
+                    );
+                  }
+                })));
   }
 }
